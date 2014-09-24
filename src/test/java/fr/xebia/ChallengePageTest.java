977135cc2c1f19c6aa7104eadf426a1604dda050
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-24 22:40:56 +0200

    rely on net.code-story.simplelenium

--- src/test/java/fr/xebia/ChallengePageTest.java
+++ src/test/java/fr/xebia/ChallengePageTest.java
@@ -1,20 +1,23 @@
 package fr.xebia;
 
-import fr.xebia.misc.PhantomJsTest;
+import net.codestory.simplelenium.SeleniumTest;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.By.className;
+import static org.openqa.selenium.By.tagName;
 
-public class ChallengePageTest extends PhantomJsTest {
+public class ChallengePageTest extends SeleniumTest {
     @ClassRule
     public static TomcatRule tomcat = new TomcatRule("0 1 true 2 3 false");
 
     @Override
-    protected String defaultUrl() {
+    protected String getDefaultBaseUrl() {
         return "http://localhost:8080";
     }
 
@@ -22,9 +25,9 @@ public class ChallengePageTest extends PhantomJsTest {
     public void restartUI() {
         goTo("/");
 
-        await().until(".guess").withText().contains(Pattern.compile("Où est [Alexandre Dergham|Anne Beauchart] ?")).isPresent();
+        find(".guess").should().match(Pattern.compile("Où est (?:Alexandre Dergham|Anne Beauchart) \\?"));
 
-        if ("Où est Anne Beauchart ?".equals($(".guess").getText())) {
+        if ("Où est Anne Beauchart ?".equals(find(".guess").getText())) {
             restartUI(); // servlet is statefull : we need to reset UI as it was when servlet was started for the first time
         }
     }
@@ -36,34 +39,34 @@ public class ChallengePageTest extends PhantomJsTest {
 
     @Test
     public void should_display_images_name_and_initial_score() {
-        assertThat($(".firstImage").find("img").getAttribute("src")).endsWith("/images/Alexandre%20Dergham.jpg");
-        assertThat($(".secondImage").find("img").getAttribute("src")).endsWith("/images/Alexandre%20Dutra.jpg");
-        assertThat($(".guess").getText()).isEqualTo("Où est Alexandre Dergham ?");
-        assertThat($(".score").find("span").getText()).isEqualTo("0");
+        assertThat(getDriver().findElement(className("firstImage")).findElement(tagName("img")).getAttribute("src")).endsWith("/images/Alexandre%20Dergham.jpg");
+        assertThat(getDriver().findElement(className("secondImage")).findElement(tagName("img")).getAttribute("src")).endsWith("/images/Alexandre%20Dutra.jpg");
+        assertThat(find(".guess").getText()).isEqualTo("Où est Alexandre Dergham ?");
+        assertThat(getDriver().findElement(className("score")).findElement(tagName("span")).getText()).isEqualTo("0");
     }
 
     @Test
     public void should_click_on_first_image_and_display_next_challenge() throws InterruptedException {
-        click(".firstImage");
+        getDriver().findElement(className("firstImage")).click();
 
-        assertThat($(".score").find("span").getText()).isEqualTo("1");
+        assertThat(getDriver().findElement(className("score")).findElement(tagName("span")).getText()).isEqualTo("1");
         Thread.sleep(2000); // TODO ugly
-        assertThat($(".firstImage").find("img").getAttribute("src")).endsWith("/images/Alexis%20Kinsella.jpg");
-        assertThat($(".secondImage").find("img").getAttribute("src")).endsWith("/images/Anne%20Beauchart.jpg");
-        assertThat($(".guess").getText()).isEqualTo("Où est Anne Beauchart ?");
+        assertThat(getDriver().findElement(className("firstImage")).findElement(tagName("img")).getAttribute("src")).endsWith("/images/Alexis%20Kinsella.jpg");
+        assertThat(getDriver().findElement(className("secondImage")).findElement(tagName("img")).getAttribute("src")).endsWith("/images/Anne%20Beauchart.jpg");
+        assertThat(find(".guess").getText()).isEqualTo("Où est Anne Beauchart ?");
     }
 
     @Test
     public void should_click_on_good_image_and_win() {
-        click(".firstImage");
+        getDriver().findElement(className("firstImage")).click();
 
-        assertThat($(".score").find("span").getText()).isEqualTo("1");
+        assertThat(getDriver().findElement(className("score")).findElement(tagName("span")).getText()).isEqualTo("1");
     }
 
     @Test
     public void should_click_on_bad_image_and_loose() {
-        click(".secondImage");
+        getDriver().findElement(className("secondImage")).click();
 
-        assertThat($(".score").find("span").getText()).isEqualTo("-1");
+        assertThat(getDriver().findElement(className("score")).findElement(tagName("span")).getText()).isEqualTo("-1");
     }
 }
